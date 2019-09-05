d09e4dbb252e38164eb2da35109b936e2d7de43c
tjvonbr <51134664+tjvonbr@users.noreply.github.com>
2019-09-05 12:32:28 -0500

    Added start script

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
 
+console.log('environment:', process.env.NODE_ENV)
+
 const apiRouter = require('./api-router.js');
 
 const server = express();
