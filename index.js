772cc48d85cc6f91bcd73bd517a27003c4b09b00
tjvonbr <51134664+tjvonbr@users.noreply.github.com>
2019-09-05 12:56:11 -0500

    Updated dynamic port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
 
-const port = 4000;
+const port = process.env.PORT;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
